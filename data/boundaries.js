85eab4cd55d8eb19fc7fe3750ea2117360737278
Jiu Axl <ajaxarrow463@gmail.com>
2023-09-23 23:12:38 +0800

    updated json

--- data/boundaries.js
+++ data/boundaries.js
@@ -1,17 +1,103 @@
-import Abra from '../geojsons/municities-province-1-abra.0.1.json'
-import Bukidnon from '../geojsons/municities-province-16-bukidnon.0.1.json'
+import abra from '../geojsons/municities-province-1-abra.0.1.json'
+import agusandelnorte from '../geojsons/municities-province-2-agusandelnorte.0.1.json'
+import agusandelsur from '../geojsons/municities-province-3-agusandelsur.0.1.json'
+import aklan from '../geojsons/municities-province-4-aklan.0.1.json'
+import albay from '../geojsons/municities-province-5-albay.0.1.json'
+import antique from '../geojsons/municities-province-6-antique.0.1.json'
+import apayao from '../geojsons/municities-province-7-apayao.0.1.json'
+import aurora from '../geojsons/municities-province-8-aurora.0.1.json'
+import basilan from '../geojsons/municities-province-9-basilan.0.1.json'
+import bataan from '../geojsons/municities-province-10-bataan.0.1.json'
+import batanes from '../geojsons/municities-province-11-batanes.0.1.json'
+import batangas from '../geojsons/municities-province-12-batangas.0.1.json'
+import benguet from '../geojsons/municities-province-13-benguet.0.1.json'
+import biliran from '../geojsons/municities-province-14-biliran.0.1.json'
+import bohol from '../geojsons/municities-province-15-bohol.0.1.json'
+import bukidnon from '../geojsons/municities-province-16-bukidnon.0.1.json'
+import bulacan from '../geojsons/municities-province-17-bulacan.0.1.json'
+import cagayan from '../geojsons/municities-province-18-cagayan.0.1.json'
+import camarinesnorte from '../geojsons/municities-province-19-camarinesnorte.0.1.json'
 
 
 const boundaries = [
     {
-        title: "Bukidnon",
-        geo_json: Bukidnon
+        title: "Abra",
+        geo_json: abra,
     },
     {
-        title: "Abra",
-        geo_json: Abra
-    }
-];
+        title: "Agusan del Norte",
+        geo_json: agusandelnorte,
+    },
+    {
+        title: "Agusan del Sur",
+        geo_json: agusandelsur,
+    },
+    {
+        title: "Aklan",
+        geo_json: aklan,
+    },
+    {
+        title: "Albay",
+        geo_json: albay,
+    },
+    {
+        title: "Antique",
+        geo_json: antique,
+    },
+    {
+        title: "Apayao",
+        geo_json: apayao,
+    },
+    {
+        title: "Aurora",
+        geo_json: aurora,
+    },
+    {
+        title: "Basilan",
+        geo_json: basilan,
+    },
+    {
+        title: "Bataan",
+        geo_json: bataan
+    },
+    {
+        title: "Batanes",
+        geo_json: batanes,
+    },
+    {
+        title: "Batangas",
+        geo_json: batangas,
+    },
+    {
+        title: "Benguet",
+        geo_json: benguet,
+    },
+    {
+        title: "Biliran",
+        geo_json: biliran,
+    },
+    {
+        title: "Bohol",
+        geo_json: bohol,
+    },
+    {
+        title: "Bukidnon",
+        geo_json: bukidnon,
+    },
+    {
+        title: "Bulacan",
+        geo_json: bulacan,
+    },
 
+    {
+        title: "Cagayan",
+        geo_json: cagayan,
+    },
+    {
+        title: "Camarines Norte",
+        geo_json: camarinesnorte,
+    }
+
+];
 
 export default boundaries;
