192998ea41f0e57b8c9d215d8af5560917da02f7
Jiu Axl <ajaxarrow463@gmail.com>
2023-09-23 21:04:21 +0800

    humana ni mer

---
+++ data/boundaries.js
@@ -0,0 +1,17 @@
+import Abra from '../geojsons/municities-province-1-abra.0.1.json'
+import Bukidnon from '../geojsons/municities-province-16-bukidnon.0.1.json'
+
+
+const boundaries = [
+    {
+        title: "Bukidnon",
+        geo_json: Bukidnon
+    },
+    {
+        title: "Abra",
+        geo_json: Abra
+    }
+];
+
+
+export default boundaries;
