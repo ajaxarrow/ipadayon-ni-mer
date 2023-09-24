fd973e9e93f6d550efd548c31105828fdb221a24
Jiu Axl <ajaxarrow463@gmail.com>
2023-09-24 17:40:41 +0800

    tiwasa ang coordinate

--- data/boundaries.js
+++ data/boundaries.js
@@ -17,86 +17,243 @@ import bukidnon from '../geojsons/municities-province-16-bukidnon.0.1.json'
 import bulacan from '../geojsons/municities-province-17-bulacan.0.1.json'
 import cagayan from '../geojsons/municities-province-18-cagayan.0.1.json'
 import camarinesnorte from '../geojsons/municities-province-19-camarinesnorte.0.1.json'
-
+import camarinessur from '../geojsons/municities-province-20-camarinessur.0.1.json'
+import camiguin from '../geojsons/municities-province-21-camiguin.0.1.json'
+import capiz from '../geojsons/municities-province-22-capiz.0.1.json'
+import catanduanes from '../geojsons/municities-province-23-catanduanes.0.1.json'
+import cavite from '../geojsons/municities-province-24-cavite.0.1.json'
+import cebu from '../geojsons/municities-province-25-cebu.0.1.json'
+import compostelavalley from '../geojsons/municities-province-26-compostelavalley.0.1.json'
+import davaodelnorte from '../geojsons/municities-province-27-davaodelnorte.0.1.json'
+import davaodelsur from '../geojsons/municities-province-28-davaodelsur.0.1.json'
+import davaooriental from '../geojsons/municities-province-29-davaooriental.0.1.json'
+import dinagatislands from '../geojsons/municities-province-30-dinagatislands.0.1.json'
+import easternsamar from '../geojsons/municities-province-31-easternsamar.0.1.json'
+import guimaras from '../geojsons/municities-province-32-guimaras.0.1.json'
+import ifugao from '../geojsons/municities-province-33-ifugao.0.1.json'
+import ilocosnorte from '../geojsons/municities-province-34-ilocosnorte.0.1.json'
+import ilocossur from '../geojsons/municities-province-35-ilocossur.0.1.json'
+import iloilo from '../geojsons/municities-province-36-iloilo.0.1.json'
+import isabela from '../geojsons/municities-province-37-isabela.0.1.json'
+import kalinga from '../geojsons/municities-province-38-kalinga.0.1.json'
+import launion from '../geojsons/municities-province-39-launion.0.1.json'
+import laguna from '../geojsons/municities-province-40-laguna.0.1.json'
+import lanaodelnorte from '../geojsons/municities-province-41-lanaodelnorte.0.1.json'
+import lanaodelsur from '../geojsons/municities-province-42-lanaodelsur.0.1.json'
 
 const boundaries = [
     {
         title: "Abra",
         geo_json: abra,
+        coordinates: [120.75, 17.58]
     },
     {
         title: "Agusan del Norte",
         geo_json: agusandelnorte,
+        coordinates: [125.45, 8.91]
     },
     {
         title: "Agusan del Sur",
         geo_json: agusandelsur,
+        coordinates: [125.70, 8.35]
     },
     {
         title: "Aklan",
         geo_json: aklan,
+        coordinates: [122.34, 11.62]
     },
     {
         title: "Albay",
         geo_json: albay,
+        coordinates: [123.549, 13.216]
     },
     {
         title: "Antique",
         geo_json: antique,
+        coordinates: [122.08, 11.17]
     },
     {
         title: "Apayao",
         geo_json: apayao,
+        coordinates: [121.19, 18.12]
     },
     {
         title: "Aurora",
         geo_json: aurora,
+        coordinates: [121.74, 16]
     },
     {
         title: "Basilan",
         geo_json: basilan,
+        coordinates: [122.03, 6.57]
     },
     {
         title: "Bataan",
-        geo_json: bataan
+        geo_json: bataan,
+        coordinates: [120.41, 14.6]
     },
     {
         title: "Batanes",
         geo_json: batanes,
+        coordinates: [121.89, 20.644]
     },
     {
         title: "Batangas",
         geo_json: batangas,
+        coordinates: [121.05, 13.7565]
     },
     {
         title: "Benguet",
         geo_json: benguet,
+        coordinates: [120.66, 16.5]
     },
     {
         title: "Biliran",
         geo_json: biliran,
+        coordinates: [124.49, 11.599]
     },
     {
         title: "Bohol",
         geo_json: bohol,
+        coordinates: [124.16, 9.833]
     },
     {
         title: "Bukidnon",
         geo_json: bukidnon,
+        coordinates: [124.99, 8.02]
     },
     {
         title: "Bulacan",
         geo_json: bulacan,
+        coordinates: [121.08, 14.999]
     },
 
     {
         title: "Cagayan",
         geo_json: cagayan,
+        coordinates: [121.833, 18]
     },
     {
         title: "Camarines Norte",
         geo_json: camarinesnorte,
-    }
+        coordinates: [122.75, 14.16]
+    },
+    {
+        title: "Camarines Sur",
+        geo_json: camarinessur,
+        coordinates: []
+    },
+    {
+        title: "Camiguin",
+        geo_json: camiguin,
+        coordinates: []
+    },
+    {
+        title: "Capiz",
+        geo_json: capiz,
+        coordinates: []
+    },
+    {
+        title: "Catanduanes",
+        geo_json: catanduanes,
+        coordinates: []
+    },
+    {
+        title: "Cavite",
+        geo_json: cavite,
+        coordinates: []
+    },
+    {
+        title: "Cebu",
+        geo_json: cebu,
+        coordinates: []
+    },
+    {
+        title: "Compostela Valley",
+        geo_json: compostelavalley,
+        coordinates: []
+    },
+    {
+        title: "Davao del Norte",
+        geo_json: davaodelnorte,
+        coordinates: []
+    },
+    {
+        title: "Davao del Sur",
+        geo_json: davaodelsur,
+        coordinates: []
+    },
+    {
+        title: "Davao Oriental",
+        geo_json: davaooriental,
+        coordinates: []
+    },
+    {
+        title: "Dinagat Islands",
+        geo_json: dinagatislands,
+        coordinates: []
+    },
+    {
+        title: "Eastern Samar",
+        geo_json: easternsamar,
+        coordinates: []
+    },
+    {
+        title: "Guimaras",
+        geo_json: guimaras,
+        coordinates: []
+    },
+    {
+        title: "Ifugao",
+        geo_json: ifugao,
+        coordinates: []
+    },
+    {
+        title: "Ilocos Norte",
+        geo_json: ilocosnorte,
+        coordinates: []
+    },
+    {
+        title: "Ilocos Sur",
+        geo_json: ilocossur,
+        coordinates: []
+    },
+    {
+        title: "Iloilo",
+        geo_json: iloilo,
+        coordinates: []
+    },
+    {
+        title: "Isabela",
+        geo_json: isabela,
+        coordinates: []
+    },
+    {
+        title: "Kalinga",
+        geo_json: kalinga,
+        coordinates: []
+    },
+    {
+        title: "La Union",
+        geo_json: launion,
+        coordinates: []
+    },
+    {
+        title: "Laguna",
+        geo_json: laguna,
+        coordinates: []
+    },
+    {
+        title: "Lanao del Norte",
+        geo_json: lanaodelnorte,
+        coordinates: []
+    },
+    {
+        title: "Lanao del Sur",
+        geo_json: lanaodelsur,
+        coordinates: []
+    },
+
 
 ];
 
